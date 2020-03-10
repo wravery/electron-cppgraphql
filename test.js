eca4e2b9c82981d5ec99d833d7087009e83b9d4e
Bill Avery <wravery@gmail.com>
2020-03-09 19:03:29 -0700

    Get tests running in electron 8.1.0

--- test.js
+++ test.js
@@ -1,4 +1,46 @@
-var graphql = require('./build/Release/electron_cppgraphqlgen');
+describe('GraphQL native module tests', () => {
+    const graphql = require('./build/Debug/electron_cppgraphql');
 
-graphql.startService();
-graphql.stopService();
+    it('starts the service', () => {
+        console.log('starts the service');
+
+        graphql.startService();
+    });
+
+    it('fetches introspection', async () => {
+        await expect(new Promise((resolve, reject) => {
+            graphql.fetchQuery(`query {
+                __schema {
+                    queryType {
+                        name
+                    }
+                    mutationType {
+                        name
+                    }
+                    subscriptionType {
+                        name
+                    }
+                    types {
+                        kind
+                        name
+                    }
+                }
+            }`, '', '', (error, response) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    try {
+                        resolve(JSON.parse(response));
+                    } catch (error) {
+                        resolve(response);
+                    }
+                }
+            });
+        })).resolves.toMatchSnapshot();
+    });
+
+    it('stops the service', () => {
+        graphql.stopService();
+    });
+
+});
